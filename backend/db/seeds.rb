c9c06b6b458928efb87bfd39e467d3b962504ed1
Aleksandra Kucharczyk-Buras <kucharczyk.aleksandra@gmail.com>
2020-01-24 22:33:01 +0100

    Add graphql gem

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+enjoyer = Enjoyer.create(name: 'Ola', email: 'ola@test.pl')
+
+checklist1 = Checklist.create(name: 'Checklist1', enjoyer_id: enjoyer.id)
+checklist2 = Checklist.create(name: 'Checklist2', enjoyer_id: enjoyer.id)
+
+Item.create(name: 'Item1', checklist_id: checklist1.id)
+Item.create(name: 'Item2', checklist_id: checklist1.id)
+Item.create(name: 'Item3', checklist_id: checklist1.id)
+Item.create(name: 'Item4', checklist_id: checklist1.id)
+Item.create(name: 'Item5', checklist_id: checklist2.id)
+Item.create(name: 'Item6', checklist_id: checklist2.id)
