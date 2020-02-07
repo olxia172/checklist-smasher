d7506cac5ebc91f8a97e82431ea204d710fe2957
Aleksandra Kucharczyk-Buras <kucharczyk.aleksandra@gmail.com>
2020-02-07 20:16:32 +0100

    Add proper setup of react navigation

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import HomeScreen from './src/screens/HomeScreen'
 
-const App = () => {
+const Stack = createStackNavigator()
+
+function App() {
   return (
-    <View><Text>Hello</Text></View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   )
 }
 
