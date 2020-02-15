4eeeb2666ebbfb2ff4f8082186dcece892a39e32
Aleksandra Kucharczyk-Buras <kucharczyk.aleksandra@gmail.com>
2020-02-15 13:07:18 +0100

    Add null constarints and defaults to items attrs

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_205005) do
+ActiveRecord::Schema.define(version: 2020_02_15_115923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,8 +44,8 @@ ActiveRecord::Schema.define(version: 2020_01_21_205005) do
     t.string "name"
     t.bigint "checklist_id", null: false
     t.date "to_do_on"
-    t.boolean "done"
-    t.boolean "cancelled"
+    t.boolean "done", default: false, null: false
+    t.boolean "cancelled", default: false, null: false
     t.bigint "item_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
