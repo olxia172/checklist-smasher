c71244de1b12786df5a8cc0928ffb69ede1bd21f
Aleksandra Kucharczyk-Buras <kucharczyk.aleksandra@gmail.com>
2020-03-22 19:52:23 +0100

    Resuce from not found error

--- backend/app/graphql/checklist_smasher_schema.rb
+++ backend/app/graphql/checklist_smasher_schema.rb
@@ -9,4 +9,17 @@ class ChecklistSmasherSchema < GraphQL::Schema
   use GraphQL::Pagination::Connections
 
   use GraphQL::Analysis::AST
+
+  use GraphQL::Execution::Errors
+
+  # err is the error that was raised during field execution, then rescued
+  # obj is the object which was having a field resolved against it
+  # args is the the Hash of arguments passed to the resolver
+  # ctx is the query context
+  # field is the GraphQL::Schema::Field instance for the field where the error was rescued
+
+  rescue_from(ActiveRecord::RecordNotFound) do |err, _obj, _args, _ctx, _field|
+    message = I18n.t('errors.not_found', record_type: err.model)
+    GraphQL::ExecutionError.new(message)
+  end
 end
